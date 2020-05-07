1520ba545df61f4f6e4bb460800b9e3e06a1465a
Daniel  Bovington <d.bovington@gmail.com>
2020-05-07 14:10:22 +0100

    set up redux store and player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Player from './features/player';
 
-function App() {
-	return App;
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Player />
+			</div>
+		);
+	}
 }
 
 export default App;
